077976e110d6493ee417d1e78ddf6e683b542a51
marcoshuck <marcoshuck@users.noreply.github.com>
2020-04-21 03:15:58 +0000

    Deploying to gh-pages from  @ e3372be6898025a5e5b737868cef7aee7d71a336 🚀

---
+++ precache-manifest.ad8adbe9fa8482218211ae25cedba820.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2833bfbb029967f4dd36",
+    "url": "/css/chunk-vendors.6bbededb.css"
+  },
+  {
+    "revision": "166a7d5503ad1b5ce21280e45ae3d115",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "c34e6eb292b823bd2468ccc1a0ff5474",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cca9e1384cd1bf4d44f",
+    "url": "/js/app.95238506.js"
+  },
+  {
+    "revision": "2833bfbb029967f4dd36",
+    "url": "/js/chunk-vendors.90445553.js"
+  },
+  {
+    "revision": "b8734a5a5246986080b42b866d15eeed",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
